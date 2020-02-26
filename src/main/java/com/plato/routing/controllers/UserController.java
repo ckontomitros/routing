c96dd226527bc8a3a1dd71133cb4640b1a63f3f0
Christos Kontomitros <christos.kontomitros@upstreamsystems.com>
2020-02-26 23:37:15 +0200

    First commit

---
+++ src/main/java/com/plato/routing/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.plato.routing.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String authenticate(){
+        return "Hello World";
+
+    }
+
+}
